4b674163dbde44eb51da50343c9803c63cceabb2
Anshu Joshi <anshu.aj.joshi@gmail.com>
2023-03-07 10:38:12 +0530

    Project init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {Navbar , Welcome , Footer , Loader , Services , Transactions} from './components/index'
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+    </div>
+  )
+}
+
+export default App
